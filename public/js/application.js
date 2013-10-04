047e30ed5312146153e9e387aec67c3d46906a5b
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-10-04 17:05:16 -0500

    Additional front page functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,27 @@
+
+
+function homeCarousel() {
+	$('#backstretch').backstretch([
+      "./images/Andrew_AAred.jpg"
+    , "./images/brown1.JPG"
+    , "./images/brown2.JPG"
+  ], {duration: 5000, fade: 1000});
+}
+
+function contentView() {
+
+
+	$(".link").on('mouseover', function(){
+		$("#aboutme").show();
+	})
+
+
+}
+
+
 $(document).ready(function() {
-	
+
+		$(".container").hide();
+	  homeCarousel();
+	  contentView();
 })
